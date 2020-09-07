d3586abc848a4f45a8a981c79a781e2c50c8aa32
R0ma-N <x6wert@gmail.com>
2020-09-07 17:04:58 +0300

    Initial commit

---
+++ Assets/Scripts/UnitAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitAnimation : MonoBehaviour {
+
+    [SerializeField] Animator animator;
+    [SerializeField] NavMeshAgent agent;
+	
+	void FixedUpdate () {
+		if (!agent.hasPath) {
+            animator.SetBool("Moving", false);
+        } else {
+            animator.SetBool("Moving", true);
+        }
+    }
+
+    //Placeholder functions for Animation events
+    void Hit() {
+    }
+
+    void FootR() {
+    }
+
+    void FootL() {
+    }
+}
