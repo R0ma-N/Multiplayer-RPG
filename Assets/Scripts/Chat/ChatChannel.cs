75d13d37db75b2c3ae5cbb5d17f8211dcf902b57
R0ma-N <x6wert@gmail.com>
2020-11-23 15:12:08 +0300

    PW7

---
+++ Assets/Scripts/Chat/ChatChannel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Networking;
+
+public class ChatChannel : NetworkBehaviour
+{
+    public new string name;
+
+    public virtual void SendFromChanel(ChatMessage message)
+    {
+        RpcSendFromChanel(message);
+    }
+
+    [ClientRpc]
+    protected void RpcSendFromChanel(ChatMessage message)
+    {
+        PlayerChat.Instance.ReciveChatMessage(message);
+    }
+
+    public virtual void SendFromPlayerChat(string text)
+    {
+        ChatMessage msg = new ChatMessage(PlayerChat.Instance.netId, NetworkInstanceId.Invalid, PlayerChat.Instance.name, text);
+        PlayerChat.Instance.CmdSendFromChannel(gameObject, msg);
+    }
+}
---
+++ Assets/Scripts/Chat/GlobalChatChannel.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class GlobalChatChannel : ChatChannel
+{
+    #region Singleton
+    public static ChatChannel Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one instance of GlobalChat found!");
+            return;
+        }
+        Instance = this;
+    }
+    #endregion
+
+}
