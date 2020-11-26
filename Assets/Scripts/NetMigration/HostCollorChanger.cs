0790ff9cf1b7a23021d87d49e9fc5b22c9bd9c3f
R0ma-N <x6wert@gmail.com>
2020-11-26 12:49:28 +0300

    HW8

---
+++ Assets/Scripts/NetMigration/HostCollorChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostCollorChanger : NetworkBehaviour
+{
+    [SerializeField] Renderer _renderer;
+    [SerializeField] Material clientMaterial;
+    [SerializeField] Material hostMaterial;
+
+    [SyncVar] bool isHost;
+
+    private void Update()
+    {
+        if (isServer)
+        {
+            if (isLocalPlayer) isHost = true;
+            else isHost = false;
+        }
+        if (isHost) _renderer.material = hostMaterial;
+        else _renderer.material = clientMaterial;
+    }
+}
---
+++ Assets/Scripts/NetMigration/SpiritsMotor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpiritsMotor : NetworkBehaviour
+{
+    [SerializeField] float speed;
+    Transform _tratsform;
+    Quaternion oldRot;
+
+    void Start()
+    {
+        _tratsform = transform;
+    }
+
+    void Update()
+    {
+        if (hasAuthority)
+        {
+            oldRot = _tratsform.localRotation;
+            _tratsform.localRotation = Quaternion.Euler(oldRot.eulerAngles.x, oldRot.eulerAngles.y + speed * Time.deltaTime, oldRot.eulerAngles.z);
+        }
+    }
+}
